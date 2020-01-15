22d54a98e00a05daa55f4884a9d40f3e2a9a5f8b
gigantor <azfaralsukor@gmail.com>
2020-01-15 22:41:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
+app.use(express.json());
 app.use(cors());
 
 const userRouter = require('./routes/user');

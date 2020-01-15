ead485ef1541a6d27089160f668e9a0ea4369381
gigantor <azfaralsukor@gmail.com>
2020-01-15 09:59:45 +0800

    revert from basic setup to rest setup

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
-// const mongoose = require('mongoose');
-const port =  process.env.PORT || 4000;
+const mongoose = require('mongoose');
+const port =  process.env.PORT || 3000;
 
-// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
-// db.on('error', (error) => console.error(error));
-// db.once('open', () => console.log('Connected to Database'));
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
-// app.use(express.json());
+app.use(express.json());
 
-// const userRouter = require('./routes/user');
-// app.use('/user', userRouter);
+const userRouter = require('./routes/user');
+app.use('/user', userRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to CDN Freelancers RESTful API Server');

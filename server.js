5301123be0b4aa241151463ad2fb2dbb418c6327
gigantor <azfaralsukor@gmail.com>
2020-01-15 12:37:37 +0800

    enable cors

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-const port =  process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-app.use(express.json());
+app.use(cors());
 
 const userRouter = require('./routes/user');
 app.use('/user', userRouter);
